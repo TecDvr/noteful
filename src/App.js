f58e78f8582b0691f8d9ac3010f7236580e37eb7
Zach Gwirtz <zachgw@gmail.com>
2019-08-17 12:38:43 -0700

    update state

--- src/App.js
+++ src/App.js
@@ -12,7 +12,9 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      ...dummyStore
+      ...dummyStore,
+      folders: [],
+      notes: []
     }
   }
 

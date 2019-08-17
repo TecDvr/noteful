d7370369271215b54bb1bcc9b3a8338aa67288b9
Zach Gwirtz <zachgw@gmail.com>
2019-08-17 12:33:51 -0700

    update

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Folder from './Folder/Folder';
 import dummyStore from './dummy-store';
 import FolderPage from './FolderPage/FolderPage';
 import NotePage from './NotePage/NotePage';
+import NotefulContext from './NotefulContext';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -21,34 +22,36 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <div className='mainPage'>
+      <NotefulContext.Provider value={this.state}>
+        <div className='mainPage'>
 
-        <header className='header'>
-          <Link className='title' style={{ textDecoration: 'none' }} to='/'>Noteful</Link>
-        </header>
+          <header className='header'>
+            <Link className='title' style={{ textDecoration: 'none' }} to='/'>Noteful</Link>
+          </header>
 
-        <Folder folders={this.state.folders} />
+          <Folder folders={this.state.folders} />
+
+          <section>
+            <Route 
+              exact
+              path='/folderpage/:id'
+              render={ (rprops) => <FolderPage folders={this.state.folders} notes={this.state.notes} {...rprops}/>} />
+          </section>
+
+          <main>
+            <Route 
+              exact
+              path='/' 
+              render={ (rprops) => <NoteList folders={this.state.folders} notes={this.state.notes} {...rprops} />} />
+            <Route 
+              exact
+              path='/notepage/:id'
+              render={ (rprops) => <NotePage folders={this.state.folders} notes={this.state.notes} {...rprops}/>} />  
+          </main>
+
+        </div>
+      </NotefulContext.Provider>
       
-        <section>
-          <Route 
-            exact
-            path='/folderpage/:id'
-            render={ (rprops) => <FolderPage folders={this.state.folders} notes={this.state.notes} {...rprops}/>} />
-        </section>
-
-        <main>
-          <Route 
-            exact
-            path='/' 
-            render={ (rprops) => <NoteList folders={this.state.folders} notes={this.state.notes} {...rprops} />} />
-          <Route 
-            exact
-            path='/notepage/:id'
-            render={ (rprops) => <NotePage folders={this.state.folders} notes={this.state.notes} {...rprops}/>} />  
-          
-        </main>
-
-      </div>
     )
   }
 }
